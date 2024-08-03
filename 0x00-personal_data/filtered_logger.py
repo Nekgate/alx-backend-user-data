f5015c450ec707ce871556f7bcda61cf6920a2d1
Nekgate <Nekgate@gmail.com>
2024-08-03 02:15:56 +0100

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,5 +4,11 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
+    """Filters a log line.
+    """
     pattern = f"{separator}({separator.join(fields)}=[^;]*)"
-    return re.sub(pattern, lambda m: f"{separator}{m.group(1).split('=')[0]}={redaction}", message)
+    return re.sub(
+                pattern,
+                lambda m: f"{separator}{m.group(1).split('=')[0]}={redaction}",
+                message
+    )

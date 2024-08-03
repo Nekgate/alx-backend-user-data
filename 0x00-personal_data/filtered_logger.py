6caeab4aea472cc3a73936f677069ab202a76360
Nekgate <Nekgate@gmail.com>
2024-08-03 03:19:25 +0100

    Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,7 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
-    """Filters a log line."""
+    """Filters a log line by redacting specified fields."""
     return re.sub(
         r'({}=)[^{}]+'.format('|'.join(fields), separator),
         r'\1' + redaction,
@@ -12,17 +12,40 @@ def filter_datum(fields, redaction, message, separator):
 
 
 class RedactingFormatter(logging.Formatter):
-    """Redacting Formatter class"""
+    """Redacting Formatter class that hides PII fields in logs."""
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
 
     def __init__(self, fields):
-        """Constructor init"""
+        """Initialize with a list of fields to redact."""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """Format Method"""
+        """Format the log record and redact PII fields."""
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), ";")
+
+
+# Define PII_FIELDS as a tuple of fields considered PII
+PII_FIELDS = ("email", "phone", "ssn", "password", "last_login")
+
+
+def get_logger() -> logging.Logger:
+    """Create and configure a logger with RedactingFormatter."""
+    from filtered_logger import RedactingFormatter
+
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    # Create a StreamHandler and set the RedactingFormatter
+    handler = logging.StreamHandler()
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    handler.setFormatter(formatter)
+
+    # Add the handler to the logger
+    logger.addHandler(handler)
+
+    return logger

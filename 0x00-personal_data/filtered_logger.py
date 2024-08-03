0188f5a4c4021ee0c07ac84a00deaf7416fbda2b
Nekgate <Nekgate@gmail.com>
2024-08-03 02:25:13 +0100

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,9 +6,7 @@ import re
 def filter_datum(fields, redaction, message, separator):
     """Filters a log line.
     """
-    pattern = f"{separator}({separator.join(fields)}=[^;]*)"
     return re.sub(
-                pattern,
-                lambda m: f"{separator}{m.group(1).split('=')[0]}={redaction}",
-                message
+        r'({}=)[^{}]+'.format('|'.join(fields),
+                              separator), r'\1' + redaction, message
     )

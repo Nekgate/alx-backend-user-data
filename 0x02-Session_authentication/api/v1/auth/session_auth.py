71dcdf096b8638b23a4c98da106ee2e6179097f1
Nekgate <Nekgate@gmail.com>
2024-08-08 20:36:20 +0100

    Use Session ID for identifying a User

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,45 +1,56 @@
 #!/usr/bin/env python3
 """Session authentication module.
 """
-from api.v1.auth.auth import Auth
 import uuid
+from typing import TypeVar, Dict
+from flask import request
+from models.user import User
+from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """Session authentication class.
-    Inherits from Auth but does not implement any methods yet
     """
-    user_id_by_session_id = {}
+    user_id_by_session_id: Dict[str, str] = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """Creates a session ID for a user_id.
-        Args:
-            user_id (str): The user ID for which to create a session.
-        Returns:
-            str: The session ID if successful.
-            None: If user_id is None or not a string.
+        """Creates a session ID for a user.
         """
-        if user_id is None or not isinstance(user_id, str):
+        if user_id is None:
             return None
 
         # Generate a new UUID for the session ID
         session_id = str(uuid.uuid4())
         # Store the session ID and associate user ID
         self.user_id_by_session_id[session_id] = user_id
-
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Returns a User ID based on a Session ID.
-        Args:
-            session_id (str): The session ID to lookup.
-        Returns:
-            str: The user ID if successful.
-            None: If session_id is None or not a string,
-            or if session_id doesn't exist.
+        """Gets the User ID based on a Session ID.
         """
-        if session_id is None or not isinstance(session_id, str):
+        if not isinstance(session_id, str):
+            return None
+        # Retrieve the user ID for the given session ID
+        return self.user_id_by_session_id.get(session_id, None)
+
+    def session_cookie(self, request=None) -> str:
+        """Gets the session cookie value from the request.
+        """
+        if request is None:
+            return None
+        session_name = getenv('SESSION_NAME', '_my_session_id')
+        return request.cookies.get(session_name, None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Gets the current user based on the session ID.
+        """
+        if request is None:
             return None
 
-        # Retrieve the user ID for the given session ID
-        return SessionAuth.user_id_by_session_id.get(session_id, None)
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+
+        if user_id is None:
+            return None
+
+        return User.get(user_id)

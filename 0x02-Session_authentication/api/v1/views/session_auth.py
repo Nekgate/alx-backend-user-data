fe8d3694403d987bcf5847278fba2e49a629279b
Nekgate <Nekgate@gmail.com>
2024-08-08 23:28:16 +0100

    New view for Session Authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,31 +1,64 @@
-# api/v1/views/session_auth.py
-from flask import Blueprint, jsonify, request, abort
+#!/usr/bin/env python3
+
+""" Flask View module that handles all routes for Session Authentication.
+"""
+
+from api.v1.views import app_views
+from flask import Flask, jsonify, request, abort
 from models.user import User
-from api.v1.app import auth
-
-session_auth_blueprint = Blueprint('session_auth', __name__)
+from os import getenv
 
 
-@session_auth_blueprint.route('/auth_session/login', methods=['POST'])
-def login():
-    """ Handle login with session authentication. """
+@app_views.route('/auth_session/login', methods=['POST'],
+                 strict_slashes=False)
+def auth_login_session():
+    """ POST /api/v1/auth_session/login
+    Handles User login
+    Return:
+      - User object JSON represented
+      - 400 if the User ID doesn't exist
+    """
     email = request.form.get('email')
     password = request.form.get('password')
 
-    if not email:
+    # check if email and password exist, if they don't, return 400
+    if email is None or email == "":
         return jsonify({"error": "email missing"}), 400
-    if not password:
+    if password is None or password == "":
         return jsonify({"error": "password missing"}), 400
 
-    user = User.search(email=email)
-    if not user:
-        return jsonify({"error": "no user found for this email"}), 404
+    # search for users with the given email
+    users = User.search({"email": email})
+    # if no users found or the list is empty, return 400
+    if not users or users == []:
+        return jsonify({"error": "no user found for this email"}), 400
+    # we iterate over the users found
+    for user in users:
+        # check if the password is valid
+        if not user.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 400
 
-    if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
+        from api.v1.app import auth  # to avoid circular import
+        # create a session for the user
+        session_id = auth.create_session(user.id)
+        # return the user object in JSON format
+        response = jsonify(user.to_json())
+        session_name = getenv("SESSION_NAME")  # set the session cookie name
+        # set the session cookie
+        response.set_cookie(session_name, session_id)
 
-    session_id = auth.create_session(user.id)
-    response = jsonify(user.to_json())
-    session_name = getenv('SESSION_NAME', '_my_session_id')
-    response.set_cookie(session_name, session_id)
-    return response
+        return response
+    # if none of the users had a valid password, return 401
+    return jsonify({"error:" "wrong password"}), 401
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def handle_logout():
+    """
+    Handle user logout
+    """
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)

71c9b5e0519fdd45704c65768615d3978bfd8561
Nekgate <Nekgate@gmail.com>
2024-08-11 00:05:15 +0100

    Expiration

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,72 +1,88 @@
 #!/usr/bin/env python3
-"""Route module for the API.
 """
-import os
+Route module for the API
+"""
 from os import getenv
+from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-
-from api.v1.views import app_views
+import os
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
-
+from models.user import User
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 auth = None
-auth_type = getenv('AUTH_TYPE', 'auth')
-if auth_type == 'auth':
+
+# We load and assign the right instance of authentication to auth
+AUTH_TYPE = getenv("AUTH_TYPE")
+
+# add auth for if AUTH_TYPE is auth
+if AUTH_TYPE == "auth":
+    from api.v1.auth.auth import Auth
     auth = Auth()
-elif auth_type == 'basic_auth':
+
+# add basic auth for if AUTH_TYPE is basic_auth
+if AUTH_TYPE == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif auth_type == 'session_auth':
+
+# add session auth for if AUTH_TYPE is session_auth
+if AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
+
+# add session exp auth for if AUTH_TYPE is session_exp_auth
+if AUTH_TYPE == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """Not found handler.
+    """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """Unauthorized handler.
+    """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """Forbidden handler.
+    """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
-def authenticate_user():
-    """Authenticates a user before processing a request.
+def before_request() -> str:
+    """ Handler before each request to validate authentication.
     """
-    if auth:
-        excluded_paths = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/',
-            '/api/v1/auth_session/login/'
-        ]
-        if auth.require_auth(request.path, excluded_paths):
-            auth_header = auth.authorization_header(request)
-            session_cookie = auth.session_cookie(request)
-            if auth_header is None and session_cookie is None:
-                abort(401)
-            request.current_user = auth.current_user(request)
-            if request.current_user is None:
-                abort(403)
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    if auth.authorization_header(request) is None and auth.session_cookie(
+                    request) is None:
+        abort(401)
+    current_user = auth.current_user(request)
+    if current_user is None:
+        abort(403)
+    request.current_user = current_user
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(debug=True, host=host, port=port)

94c92b1b5236bf9853247c192a246747ba352654
Nekgate <Nekgate@gmail.com>
2024-08-14 19:29:33 +0100

    Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ Route module for the API """
-
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request,  abort, redirect, url_for
+from sqlalchemy.orm.exc import NoResultFound
 from auth import Auth
+
 
 app = Flask(__name__)
 AUTH = Auth()

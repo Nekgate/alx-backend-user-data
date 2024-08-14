40cdc3197f9c4d1fcb294e341553190cae3000b6
Nekgate <Nekgate@gmail.com>
2024-08-14 19:05:19 +0100

    Basic Flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,10 +5,12 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def index():
     """Return a JSON response with a welcome message."""
     return jsonify(message="Bienvenue")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

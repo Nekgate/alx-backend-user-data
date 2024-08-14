293e004032ba5ee776491ee54ffec01818f89874
Nekgate <Nekgate@gmail.com>
2024-08-14 18:34:45 +0100

    Hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 """Authentication
 """
-import bcrypt
+from bcrypt import hashpw, gensalt, checkpw
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from uuid import uuid4
+from typing import Union
+
 
 def _hash_password(password: str) -> str:
     """ Takes in string arg, converts to unicode

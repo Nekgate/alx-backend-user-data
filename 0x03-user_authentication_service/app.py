e99f7098b0d5efaa5e92be1774975d598be070f1
Nekgate <Nekgate@gmail.com>
2024-08-14 19:28:09 +0100

    Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,14 +2,45 @@
 """ Route module for the API """
 
 from flask import Flask, jsonify
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
-@app.route("/", methods=["GET"])
-def index():
-    """Return a JSON response with a welcome message."""
-    return jsonify(message="Bienvenue")
+@app.route('/', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET /status
+    Return:
+      - JSON payload
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def new_user() -> str:
+    """ POST /users
+    Registers new user with email and pswd in request form-data,
+    or finds if user already registered based on email
+    Return:
+      - JSON payload
+    """
+
+    # Get data from form request, change to request.get_json() for body
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        new_user = AUTH.register_user(email, password)
+        if new_user is not None:
+            return jsonify({
+                "email": new_user.email,
+                "message": "user created"
+            })
+    except ValueRrror:
+        return jsonify({
+            "message": "email already registered"
+            }), 400
 
 
 if __name__ == "__main__":
